90a86f4370e5ec8bed5af95c69e4164cc51404b9
Mehul Mohan <mehulmpt@gmail.com>
2020-06-14 20:47:03 +0530

    Passing data to modal

--- src/components/JoinTeam/Jointeam.js
+++ src/components/JoinTeam/Jointeam.js
@@ -1,62 +1,45 @@
-import React from 'react';
-import './Jointeam.scss';
-import Modal from '../Modal/Modal';
+import React from 'react'
+import './Jointeam.scss'
+import Modal from '../Modal/Modal'
 
 const Jointeam = (props) => {
-    
-    const modalRef = React.useRef();
-    
-    const openModal = () => {
-        modalRef.current.openModal();
-    }
-    
-    return (
-        
-        <React.Fragment>
+	const modalRef = React.useRef()
 
+	const openModal = (data) => {
+		modalRef.current.openModal(data)
+	}
 
-            <h1>Join the team!</h1>
-            
-            <Modal ref={modalRef}>
+	return (
+		<React.Fragment>
+			<h1>Join the team!</h1>
 
-            </Modal>
-            <div className="container">
-
-                <div className="box-positions">
-                    <div className="positions">
-
-                        <div className="singular">
-
-                            <h1>lorem adsadsa</h1>
-                            <button onClick={openModal}><a>OPEN</a></button>
-                            
-                        </div>
-                        <div  className="singular">
-
-                            <h1>lorem dsadsadsadsa</h1>
-                            <button onClick={openModal}>OPEN</button>
-
-                        </div>
-                        <div  className="singular">
-
-                            <h1>lorem dasdasdsasaddsa</h1>
-                            <button onClick={openModal}>OPEN</button>
-
-                        </div>
-                        <div  className="singular">
-
-                            <h1>lorem dasdsasdsa</h1>
-                            <button>CLOSED</button>
-
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </React.Fragment>
-
-    );
-
+			<Modal ref={modalRef}></Modal>
+			<div className="container">
+				<div className="box-positions">
+					<div className="positions">
+						<div className="singular">
+							<h1>lorem adsadsa</h1>
+							<button onClick={() => openModal({ title: 'Title 1' })}>
+								<a>OPEN</a>
+							</button>
+						</div>
+						<div className="singular">
+							<h1>lorem dsadsadsadsa</h1>
+							<button onClick={() => openModal({ title: 'Title 2' })}>OPEN</button>
+						</div>
+						<div className="singular">
+							<h1>lorem dasdasdsasaddsa</h1>
+							<button onClick={() => openModal({ title: 'Title 3' })}>OPEN</button>
+						</div>
+						<div className="singular">
+							<h1>lorem dasdsasdsa</h1>
+							<button>CLOSED</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	)
 }
 
-export default Jointeam;
+export default Jointeam
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,45 +1,40 @@
-import React, { forwardRef, useImperativeHandle,useState } from "react";
-import ReactDOM from "react-dom";
+import React, { forwardRef, useImperativeHandle, useState } from 'react'
+import ReactDOM from 'react-dom'
 //import { PositionData } from "../../PositionData";
 
 const Modal = forwardRef((props, ref) => {
+	const [display, setDisplay] = React.useState(false)
+	const [popupData, setPopupData] = React.useState(null)
 
-    const [display, setDisplay] = React.useState(false);
-    
+	useImperativeHandle(ref, () => {
+		return {
+			openModal: open,
+			close: () => close()
+		}
+	})
 
-    useImperativeHandle(ref, () => {
-        return {
-            openModal: () => open(),
-            close: () => close()
-        }
-    })
+	const open = (data) => {
+		setDisplay(true)
+		setPopupData(data)
+	}
+	//
+	const close = () => {
+		setDisplay(false)
+		setPopupData(null)
+	}
 
-    const open = () => {
-        setDisplay(true)
-        
-    }
-//
-    const close = () => {
-        setDisplay(false);
-    }
-    
-  
+	if (display) {
+		return ReactDOM.createPortal(
+			<div className="new-position-wrapper">
+				<div onClick={close} className={'modal-backdrop'} />
+				<div className={'modal-box'}>
+					<h1>{popupData?.title || 'No title specified'}</h1>
+				</div>
+			</div>,
+			document.getElementById('root')
+		)
+	}
+	return null
+})
 
-   
-    
-    if (display) {
-        
-        return ReactDOM.createPortal(
-            <div className="new-position-wrapper">
-                <div onClick={close} className={"modal-backdrop"} />
-                <div className={"modal-box"}>
-                  <h1>asd</h1>
-                </div>
-            </div>, document.getElementById("root"))
-
-    }
-    return null;
-});
-
-
-export default Modal;
+export default Modal
